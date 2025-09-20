d52d477d526d34f2d52ccef9a1b80374739d336c
Siddesh_G_H <1nt24is216.siddesh@nmit.ac.in>
2025-09-20 12:52:57 +0530

    All files are added to track

---
+++ src/test/java/com/nmit/Ecom_service/EcomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmit.Ecom_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
